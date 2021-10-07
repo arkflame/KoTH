b17b4d344d772c2a1c77fbe5f725e15c0c782288
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-10-07 17:56:12 -0300

    update source

--- src/main/java/dev/_2lstudios/koth/koth/KothEvent.java
+++ src/main/java/dev/_2lstudios/koth/koth/KothEvent.java
@@ -1,12 +1,8 @@
 package dev._2lstudios.koth.koth;
 
-import dev._2lstudios.koth.KothPlugin;
-import dev._2lstudios.koth.schedule.KothSchedule;
-import dev._2lstudios.koth.schedule.KothScheduleManager;
-import dev._2lstudios.koth.utils.ConfigurationUtil;
 import java.util.Collection;
 import java.util.HashSet;
-import org.bukkit.Bukkit;
+
 import org.bukkit.ChatColor;
 import org.bukkit.Color;
 import org.bukkit.FireworkEffect;
@@ -24,16 +20,29 @@ import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.util.Vector;
 
+import dev._2lstudios.koth.KothPlugin;
+import dev._2lstudios.koth.schedule.KothSchedule;
+import dev._2lstudios.koth.schedule.KothScheduleManager;
+import dev._2lstudios.koth.utils.ConfigurationUtil;
+
 public class KothEvent {
   private final Plugin plugin;
   private final ConfigurationUtil configurationUtil;
   private final String name;
   private final Collection<KothPlayer> kothPlayers;
-  private Location position1;
+
+  private String worldName;
+  private Vector position1;
+  private Vector position2;
+
+  private ItemStack[] rewards;
+  private long lastStartTime;
+  private int captureTime;
+  private int maxTime;
+  private boolean running;
 
   KothEvent(final Plugin plugin, final ConfigurationUtil configurationUtil, final Collection<KothPlayer> kothPlayers,
       final String name) {
-    World world;
     this.position1 = null;
     this.position2 = null;
     this.running = false;
@@ -49,26 +58,19 @@ public class KothEvent {
     final int maxTime = yamlConfiguration.getInt("maxtime");
     final int captureTime = yamlConfiguration.getInt("capturetime");
 
-    if (worldName != null) {
-      world = Bukkit.getWorld(worldName);
-    } else {
-      world = null;
-    }
-    if (rewardsSection != null)
+    if (rewardsSection != null) {
       for (final String rewardKey : rewardsSection.getKeys(false)) {
         rewards.add(yamlConfiguration.getItemStack("rewards." + rewardKey));
       }
+    }
+
     this.configurationUtil = configurationUtil;
     this.kothPlayers = kothPlayers;
     this.name = name;
+    this.worldName = worldName;
+    this.position1 = vector1;
+    this.position1 = vector2;
 
-    if (world != null) {
-      if (vector1 != null)
-        this.position1 = new Location(world, vector1.getX(), vector1.getY(), vector1.getZ());
-      if (vector2 != null) {
-        this.position2 = new Location(world, vector2.getX(), vector2.getY(), vector2.getZ());
-      }
-    }
     if (!rewards.isEmpty()) {
       this.rewards = rewards.<ItemStack>toArray(new ItemStack[0]);
     }
@@ -77,19 +79,12 @@ public class KothEvent {
     this.lastStartTime = System.currentTimeMillis();
   }
 
-  private Location position2;
-  private ItemStack[] rewards;
-  private long lastStartTime;
-  private int captureTime;
-  private int maxTime;
-  private boolean running;
-
   public boolean isRunning() {
     return this.running;
   }
 
   public String getWorldName() {
-    return this.position1.getWorld().getName();
+    return worldName;
   }
 
   public int getCenterX() {
@@ -107,9 +102,9 @@ public class KothEvent {
     final int z = getCenterZ();
 
     if (topPlayer == null) {
-      capturing = "\n&eNadie esta capturando el evento de &b&lKoTH &econ premios!\n";
+      capturing = "\n&eNadie esta capturando el evento de &b&lKoTH&e!\n";
     } else {
-      capturing = "\n&eEstan capturando el evento de &b&lKoTH &econ premios!&r\n";
+      capturing = "\n&eEstan capturando el evento de &b&lKoTH&e!&r\n";
     }
 
     this.plugin.getServer()
@@ -154,10 +149,8 @@ public class KothEvent {
       }
 
       if (winner == null) {
-        server.broadcastMessage(
-            ChatColor.translateAlternateColorCodes('&', "\n&eEl evento de &b&lKoTH &econ premios ha finalizado!"
-
-                .concat("\n&cNo hay ganadores del evento!\n")));
+        server.broadcastMessage(ChatColor.translateAlternateColorCodes('&',
+            "\n&eEl evento de &b&lKoTH &econ premios ha finalizado!\n&cNo hay ganadores del evento!\n"));
       } else {
         final Location winnerLocation = winner.getLocation();
 
@@ -199,13 +192,15 @@ public class KothEvent {
     save();
   }
 
-  public void setPosition1(final Location location1) {
-    this.position1 = location1;
+  public void setPosition1(final Location location) {
+    this.worldName = location.getWorld().getName();
+    this.position1 = location.toVector();
     save();
   }
 
-  public void setPosition2(final Location location2) {
-    this.position2 = location2;
+  public void setPosition2(final Location location) {
+    this.worldName = location.getWorld().getName();
+    this.position2 = location.toVector();
     save();
   }
 
@@ -219,8 +214,8 @@ public class KothEvent {
   }
 
   public boolean isInside(final Location location) {
-    if (this.position1 != null && this.position2 != null && this.position1.getWorld() == location.getWorld()
-        && this.position2.getWorld() == location.getWorld()) {
+    if (this.position1 != null && this.position2 != null && worldName != null
+        && worldName.equals(location.getWorld().getName())) {
       return (isBetween(location.getX(), this.position1.getX(), this.position2.getX())
           && isBetween(location.getY(), this.position1.getY(), this.position2.getY())
           && isBetween(location.getZ(), this.position1.getZ(), this.position2.getZ()));
@@ -248,13 +243,11 @@ public class KothEvent {
   private void save() {
     final YamlConfiguration yamlConfiguration = new YamlConfiguration();
 
-    if (this.position1 != null) {
-      yamlConfiguration.set("locations.world", this.position1.getWorld().getName());
-      yamlConfiguration.set("locations.vector1", this.position1.toVector());
-    }
+    yamlConfiguration.set("locations.world", worldName);
 
-    if (this.position2 != null)
-      yamlConfiguration.set("locations.vector2", this.position2.toVector());
+    yamlConfiguration.set("locations.vector1", this.position1);
+    yamlConfiguration.set("locations.vector2", this.position2);
+
     yamlConfiguration.set("capturetime", Integer.valueOf(this.captureTime));
     yamlConfiguration.set("maxtime", Integer.valueOf(this.maxTime));
 
